3cc32749d58437bfd67eb69e549123351eff83c8
sou31415 <tbc3141@icloud.com>
2023-07-08 18:38:25 +0900

    modified inter.rs

--- src/inter.rs
+++ src/inter.rs
@@ -14,17 +14,15 @@ pub fn power(n: usize, mut x: usize) -> usize {
     }
     a
 }
-pub fn powm(n: usize, m: usize, c: usize) -> usize {
-    let k: usize = 1;
-    let mut x = c;
+pub fn powm(n: usize, m: usize, mut x: usize) -> usize {
     let mut b: usize = n;
     let mut a: usize = 1;
     let mut i = 0;
     while x != 0 {
-        if k << i & x != 0 {
+        if 1usize << i & x != 0 {
             a = (a * b) % m;
             b = (b * b) % m;
-            x ^= k << i;
+            x ^= 1usize << i;
         } else {
             b = (b * b) % m;
         }
