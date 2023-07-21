e1a3f146e362e449093cd704dd50aaf1bf79f99e
sou31415 <tbc3141@icloud.com>
2023-07-21 11:31:13 +0900

    modified matrix.rs

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,5 +1,8 @@
-pub fn matrix_pow(mut r:Vec<Vec<usize>> , a: usize, m: usize, mut x: usize) -> Vec<Vec<usize>> {
-    let mut v: Vec<Vec<usize>> = vec![vec![1, 0], vec![0, 1]];
+pub fn matrix_pow(mut r: Vec<Vec<usize>>, a: usize, m: usize, mut x: usize) -> Vec<Vec<usize>> {
+    let mut v: Vec<Vec<usize>> = vec![vec![0; r.len()]; r.len()];
+    for i in 0..r.len() {
+        v[i][i] = 1;
+    }
     let mut i: usize = 0;
     while x != 0 {
         if 1usize << i & x != 0 {
