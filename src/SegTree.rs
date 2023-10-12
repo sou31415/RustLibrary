c6bef53298d93981d20d614d2896c74e78834e95
sou31415 <tbc3141@icloud.com>
2023-10-12 23:15:16 +0900

    add SegmentTree structure

---
+++ src/SegTree.rs
@@ -0,0 +1,12 @@
+pub struct SegTree {
+    pub tree:Vec<usize>,
+    pub Monoid:usize,
+    pub eval:F,
+}
+
+impl <F:Fn(usize,usize) -> usize> SegmentTree<F>{
+    fn from(m:usize,d:Vec<usize> , formula:F) -> SegTree {
+        let size = d.len().next_power_of_two();
+        let mut v = vec![m;size*2
+    }
+}
