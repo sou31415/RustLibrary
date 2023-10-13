f5c7f3237f69fc835bd3efa4266b82b7bd04bdb4
sou31415 <tbc3141@icloud.com>
2023-10-13 11:29:28 +0900

    modified Generics Segtree

--- src/SegTree.rs
+++ src/SegTree.rs
@@ -1,12 +1,12 @@
 pub struct SegTree {
-    pub tree:Vec<usize>,
-    pub Monoid:usize,
-    pub eval:F,
+    pub tree: Vec<usize>,
+    pub Monoid: usize,
+    pub eval: F,
 }
 
-impl <F:Fn(usize,usize) -> usize> SegmentTree<F>{
-    fn from(m:usize,d:Vec<usize> , formula:F) -> SegTree {
+impl<F: Fn(usize, usize) -> usize> SegmentTree<F> {
+    fn from(m: usize, d: Vec<usize>, formula: F) -> SegTree<F> {
         let size = d.len().next_power_of_two();
-        let mut v = vec![m;size*2
+        let mut v = vec![m; size * 2];
     }
 }
